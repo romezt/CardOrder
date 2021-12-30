027f5a77f510200ffd7a232928917654b4676ec5
Anton <ikkii780@mail.ru>
2021-12-30 11:53:36 +0500

    Initial commit

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @Test
+    public void shouldSendForm() {
+        open("http://localhost:9999");
+        $(".input_type_text input").setValue("Игорь Николаев");
+        $(".input_type_tel input").setValue("+79113334554");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("p").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
